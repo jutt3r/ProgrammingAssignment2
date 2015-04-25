a4944262cefbb42870eacd983e5b0685d5e80a50
jutt3r <jutter@gmx.net>
2015-04-25 10:46:46 -0400

    updated cachematrix.R to complete assignment

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,46 @@
-## Put comments here that give an overall description of what your
-## functions do
+## makeCacheMatrix and cacheSolve let you study the scoping rules in R
 
-## Write a short comment describing this function
+## i initialises an empty object
+## set defines a function and introduces the superassignment operator: <<-
+### set sets x to y and i to NULL in the *parent environment*
+## get defines a function that will return x
+## setinv defines a function that will calculate the inverse of a matrix and
+### store the output as i in the parent environment (here = makeCacheMatrix env)
+## getinv defines a function that will return i
+## list will assign the four functions to the object of makeCacheMatrix
+
 
 makeCacheMatrix <- function(x = matrix()) {
-
+    i <- NULL
+    set <- function(y) {
+        x <<- y
+        i <<- NULL
+    }
+    get <- function() x
+    setinv <- function(solve) i <<- solve
+    getinv <- function() i
+    list(set = set, get = get,
+         setinv = setinv,
+         getinv = getinv)
 }
 
-
-## Write a short comment describing this function
+## cacheSolve returns a matrix that is the inverse of 'x'
+## i is set to be the inverse of x
+## the if-loop checks if there is already a value assigned to i and returns it
+### in case there is one
+## if i is NULL, data retrieves the original matrix
+## i is then calculated as the inverse of x
+## x$setinv(i) assigns the new i
+## finally i is returned
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+    i <- x$getinv()
+    if(!is.null(i)) {
+        message("getting cached data")
+        return(i)
+    }
+    data <- x$get()
+    i <- solve(data, ...)
+    x$setinv(i)
+    i
 }
